e6c82f7c04692f4c4393bc55cf608d6d3a6117da
k141303 <kouta.nakayama@gmail.com>
2018-07-15 21:15:38 +0900

    chainの変更
    ネットワーク構成の整理

--- chain.py
+++ chain.py
@@ -8,7 +8,6 @@ import chainer.functions as F
 
 class MyChain(Chain):
     def __init__(self,in_units,hd_units,out_units):
-
         super(MyChain, self).__init__(
             l1 = L.NStepLSTM(n_layers=2, in_size=in_units,
                       out_size=hd_units, dropout=0.5),
@@ -19,15 +18,6 @@ class MyChain(Chain):
     def predict(self, x):
         _,_,ys = self.l1(None,None,x)
         h = F.vstack(map(lambda y: y[-1], ys))
-        h2 = F.dropout(F.relu(self.l2(h)))
+        h2 = F.relu(self.l2(h))
         out = F.softmax(self.l3(h2))
         return  out
-
-    def reset(self):
-        pass
-        #self.l1.reset_state()
-
-    def output(self,x):
-        h1 = self.l1(x)
-        h2 = self.l2(h1)
-        return  self.l3(h2)
